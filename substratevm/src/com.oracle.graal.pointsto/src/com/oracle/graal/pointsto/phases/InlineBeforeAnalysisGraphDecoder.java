hpi-swa-lab/graalvm-causality-graph

CG-Improvement: Fix transitive InlineBeforeAnalysis

substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/phases/InlineBeforeAnalysisGraphDecoder.java
@@ -227,7 +227,7 @@ public class InlineBeforeAnalysisGraphDecoder<S extends InlineBeforeAnalysisPoli
         }
         Object reason = graph.currentNodeSourcePosition() != null ? graph.currentNodeSourcePosition() : graph.method();
 
-        CausalityExport.get().registerEdge(new CausalityExport.MethodReachable((AnalysisMethod) graph.method()), new CausalityExport.MethodReachable((AnalysisMethod) invokeData.callTarget.targetMethod()));
+        CausalityExport.get().registerEdge(new CausalityExport.MethodReachable((AnalysisMethod) callerScope.method), new CausalityExport.MethodReachable((AnalysisMethod) invokeData.callTarget.targetMethod()));
         ((AnalysisMethod) invokeData.callTarget.targetMethod()).registerAsInlined(reason);
 
         super.finishInlining(inlineScope);

hpi-swa-lab/graalvm-causality-graph-backup

substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/phases/InlineBeforeAnalysisGraphDecoder.java
@@ -227,7 +227,7 @@ public class InlineBeforeAnalysisGraphDecoder<S extends InlineBeforeAnalysisPoli
         }
         Object reason = graph.currentNodeSourcePosition() != null ? graph.currentNodeSourcePosition() : graph.method();
 
-        CausalityExport.get().registerEdge(new CausalityExport.MethodCode((AnalysisMethod) graph.method()), new CausalityExport.MethodReachable((AnalysisMethod) invokeData.callTarget.targetMethod()));
+        CausalityExport.get().registerEdge(new CausalityExport.MethodCode((AnalysisMethod) callerScope.method), new CausalityExport.MethodReachable((AnalysisMethod) invokeData.callTarget.targetMethod()));
         ((AnalysisMethod) invokeData.callTarget.targetMethod()).registerAsInlined(reason);
 
         super.finishInlining(inlineScope);