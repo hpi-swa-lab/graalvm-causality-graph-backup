hpi-swa-lab/graalvm-causality-graph

FlatReachabilityExporter: Export more facts

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/FlatReachabilityExporter.java
@@ -32,14 +32,18 @@ import com.oracle.svm.core.BuildArtifacts.ArtifactType;
 import com.oracle.svm.core.SubstrateOptions;
 import com.oracle.svm.core.feature.AutomaticallyRegisteredFeature;
 import com.oracle.svm.core.feature.InternalFeature;
+import com.oracle.svm.core.hub.ClassForNameSupport;
 import com.oracle.svm.core.option.HostedOptionValues;
+import com.oracle.svm.hosted.jni.JNIAccessFeature;
+import com.oracle.svm.hosted.reflect.ReflectionHostedSupport;
+import org.graalvm.nativeimage.ImageSingletons;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 @AutomaticallyRegisteredFeature
@@ -58,54 +62,88 @@ public class FlatReachabilityExporter implements InternalFeature {
             .generatedFiles(HostedOptionValues.singleton())
             .resolve("reachable_methods.txt");
 
+    public final Path reflectionAccessibleMethodsPath = NativeImageGenerator
+            .generatedFiles(HostedOptionValues.singleton())
+            .resolve("reflection_methods.txt");
+
+    public final Path reflectionAccessibleTypesPath = NativeImageGenerator
+            .generatedFiles(HostedOptionValues.singleton())
+            .resolve("reflection_types.txt");
+
+    public final Path jniAccessibleTypesPath = NativeImageGenerator
+            .generatedFiles(HostedOptionValues.singleton())
+            .resolve("jni_types.txt");
+    public final Path jniAccessibleMethodsPath = NativeImageGenerator
+            .generatedFiles(HostedOptionValues.singleton())
+            .resolve("jni_methods.txt");
+
     @Override
     public boolean isInConfiguration(IsInConfigurationAccess access) {
         return SubstrateOptions.GenerateFlatReachability.getValue();
+    }
+
+    private static void writeList(Path path, Stream<String> lines) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()))) {
+            for(String line : (Iterable<String>) lines::iterator) {
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+        BuildArtifacts.singleton().add(ArtifactType.BUILD_INFO, path);
     }
 
     @Override
     public void afterAnalysis(AfterAnalysisAccess access) {
         FeatureImpl.AfterAnalysisAccessImpl accessImpl = (FeatureImpl.AfterAnalysisAccessImpl) access;
 
-        try (
-                BufferedWriter reachableTypes = new BufferedWriter(new FileWriter(reachableTypesPath.toFile()));
-                BufferedWriter instantiatedTypes = new BufferedWriter(new FileWriter(instantiatedTypesPath.toFile()))
-        ) {
-            ArrayList<AnalysisType> types = new ArrayList<>(accessImpl.getUniverse().getTypes());
-            types.sort(Comparator.comparing(CausalityExport::stableTypeName));
-
-            for (AnalysisType t : types) {
-                if (t.isReachable()) {
-                    reachableTypes.write(CausalityExport.stableTypeName(t));
-                    reachableTypes.newLine();
-                }
-                if (t.isInstantiated()) {
-                    instantiatedTypes.write(CausalityExport.stableTypeName(t));
-                    instantiatedTypes.newLine();
-                }
-            }
+        try {
+            writeList(reachableTypesPath,
+                    accessImpl.getUniverse().getTypes().stream()
+                            .filter(AnalysisType::isReachable)
+                            .map(CausalityExport::stableTypeName)
+                            .sorted());
+            writeList(instantiatedTypesPath,
+                    accessImpl.getUniverse().getTypes().stream()
+                            .filter(AnalysisType::isInstantiated)
+                            .map(CausalityExport::stableTypeName)
+                            .sorted());
+            writeList(reachableMethodsPath,
+                    accessImpl
+                            .getUniverse()
+                            .getMethods()
+                            .stream()
+                            .filter(AnalysisMethod::isReachable)
+                            .map(CausalityExport::stableMethodName)
+                            .sorted());
+            writeList(reflectionAccessibleTypesPath,
+                    Arrays.stream(ClassForNameSupport.getSuccessfullyRegisteredClasses())
+                            .map(accessImpl.getBigBang().getMetaAccess()::optionalLookupJavaType)
+                            .filter(Optional::isPresent)
+                            .map(Optional::get)
+                            .filter(AnalysisType::isReachable)
+                            .map(CausalityExport::stableTypeName)
+                            .sorted());
+            writeList(reflectionAccessibleMethodsPath,
+                    ImageSingletons.lookup(ReflectionHostedSupport.class).getReflectionExecutables().keySet().stream()
+                            .filter(AnalysisMethod::isReachable)
+                            .map(CausalityExport::stableMethodName)
+                            .sorted());
+            writeList(jniAccessibleTypesPath,
+                    Arrays.stream(JNIAccessFeature.singleton().getRegisteredClasses())
+                            .map(accessImpl.getBigBang().getMetaAccess()::optionalLookupJavaType)
+                            .filter(Optional::isPresent)
+                            .map(Optional::get)
+                            .filter(AnalysisType::isReachable)
+                            .map(CausalityExport::stableTypeName)
+                            .sorted());
+            writeList(jniAccessibleMethodsPath,
+                    Arrays.stream(JNIAccessFeature.singleton().getRegisteredMethods())
+                            .map(accessImpl.getUniverse()::lookup)
+                            .filter(AnalysisMethod::isReachable)
+                            .map(CausalityExport::stableMethodName)
+                            .sorted());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        BuildArtifacts.singleton().add(ArtifactType.BUILD_INFO, reachableTypesPath);
-        BuildArtifacts.singleton().add(ArtifactType.BUILD_INFO, instantiatedTypesPath);
-
-        try (BufferedWriter methods = new BufferedWriter(new FileWriter(reachableMethodsPath.toFile()))) {
-            Stream<String> names = accessImpl
-                    .getUniverse()
-                    .getMethods()
-                    .stream()
-                    .filter(AnalysisMethod::isReachable)
-                    .map(CausalityExport::stableMethodName)
-                    .sorted();
-
-            for(String name : (Iterable<String>) names::iterator) {
-                methods.write(name);
-                methods.newLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        BuildArtifacts.singleton().add(ArtifactType.BUILD_INFO, reachableMethodsPath);
     }
 }

hpi-swa-lab/graalvm-causality-graph-backup

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/FlatReachabilityExporter.java
@@ -7,19 +7,23 @@ import com.oracle.svm.core.BuildArtifacts.ArtifactType;
 import com.oracle.svm.core.SubstrateOptions;
 import com.oracle.svm.core.feature.AutomaticallyRegisteredFeature;
 import com.oracle.svm.core.feature.InternalFeature;
+import com.oracle.svm.core.hub.ClassForNameSupport;
 import com.oracle.svm.core.option.HostedOptionValues;
+import com.oracle.svm.hosted.jni.JNIAccessFeature;
+import com.oracle.svm.hosted.reflect.ReflectionHostedSupport;
 import jdk.vm.ci.meta.JavaField;
 import jdk.vm.ci.meta.JavaMethod;
 import jdk.vm.ci.meta.JavaType;
 import jdk.vm.ci.meta.MetaUtil;
 import jdk.vm.ci.meta.Signature;
+import org.graalvm.nativeimage.ImageSingletons;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 @AutomaticallyRegisteredFeature
@@ -38,55 +42,89 @@ public class FlatReachabilityExporter implements InternalFeature {
             .generatedFiles(HostedOptionValues.singleton())
             .resolve("reachable_methods.txt");
 
+    public final Path reflectionAccessibleMethodsPath = NativeImageGenerator
+            .generatedFiles(HostedOptionValues.singleton())
+            .resolve("reflection_methods.txt");
+
+    public final Path reflectionAccessibleTypesPath = NativeImageGenerator
+            .generatedFiles(HostedOptionValues.singleton())
+            .resolve("reflection_types.txt");
+
+    public final Path jniAccessibleTypesPath = NativeImageGenerator
+            .generatedFiles(HostedOptionValues.singleton())
+            .resolve("jni_types.txt");
+    public final Path jniAccessibleMethodsPath = NativeImageGenerator
+            .generatedFiles(HostedOptionValues.singleton())
+            .resolve("jni_methods.txt");
+
     @Override
     public boolean isInConfiguration(IsInConfigurationAccess access) {
         return SubstrateOptions.GenerateFlatReachability.getValue();
+    }
+
+    private static void writeList(Path path, Stream<String> lines) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()))) {
+            for(String line : (Iterable<String>) lines::iterator) {
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+        BuildArtifacts.singleton().add(ArtifactType.BUILD_INFO, path);
     }
 
     @Override
     public void afterAnalysis(AfterAnalysisAccess access) {
         FeatureImpl.AfterAnalysisAccessImpl accessImpl = (FeatureImpl.AfterAnalysisAccessImpl) access;
 
-        try (
-                BufferedWriter reachableTypes = new BufferedWriter(new FileWriter(reachableTypesPath.toFile()));
-                BufferedWriter instantiatedTypes = new BufferedWriter(new FileWriter(instantiatedTypesPath.toFile()))
-        ) {
-            ArrayList<AnalysisType> types = new ArrayList<>(accessImpl.getUniverse().getTypes());
-            types.sort(Comparator.comparing(FlatReachabilityExporter::stableTypeName));
-
-            for (AnalysisType t : types) {
-                if (t.isReachable()) {
-                    reachableTypes.write(FlatReachabilityExporter.stableTypeName(t));
-                    reachableTypes.newLine();
-                }
-                if (t.isInstantiated()) {
-                    instantiatedTypes.write(FlatReachabilityExporter.stableTypeName(t));
-                    instantiatedTypes.newLine();
-                }
-            }
+        try {
+            writeList(reachableTypesPath,
+                    accessImpl.getUniverse().getTypes().stream()
+                            .filter(AnalysisType::isReachable)
+                            .map(FlatReachabilityExporter::stableTypeName)
+                            .sorted());
+            writeList(instantiatedTypesPath,
+                    accessImpl.getUniverse().getTypes().stream()
+                            .filter(AnalysisType::isInstantiated)
+                            .map(FlatReachabilityExporter::stableTypeName)
+                            .sorted());
+            writeList(reachableMethodsPath,
+                    accessImpl
+                            .getUniverse()
+                            .getMethods()
+                            .stream()
+                            .filter(AnalysisMethod::isReachable)
+                            .map(FlatReachabilityExporter::stableMethodName)
+                            .sorted());
+            writeList(reflectionAccessibleTypesPath,
+                    Arrays.stream(ClassForNameSupport.getSuccessfullyRegisteredClasses())
+                            .map(accessImpl.getBigBang().getMetaAccess()::optionalLookupJavaType)
+                            .filter(Optional::isPresent)
+                            .map(Optional::get)
+                            .filter(AnalysisType::isReachable)
+                            .map(FlatReachabilityExporter::stableTypeName)
+                            .sorted());
+            writeList(reflectionAccessibleMethodsPath,
+                    ImageSingletons.lookup(ReflectionHostedSupport.class).getReflectionExecutables().keySet().stream()
+                            .filter(AnalysisMethod::isReachable)
+                            .map(FlatReachabilityExporter::stableMethodName)
+                            .sorted());
+            writeList(jniAccessibleTypesPath,
+                    Arrays.stream(JNIAccessFeature.singleton().getRegisteredClasses())
+                            .map(accessImpl.getBigBang().getMetaAccess()::optionalLookupJavaType)
+                            .filter(Optional::isPresent)
+                            .map(Optional::get)
+                            .filter(AnalysisType::isReachable)
+                            .map(FlatReachabilityExporter::stableTypeName)
+                            .sorted());
+            writeList(jniAccessibleMethodsPath,
+                    Arrays.stream(JNIAccessFeature.singleton().getRegisteredMethods())
+                            .map(accessImpl.getUniverse()::lookup)
+                            .filter(AnalysisMethod::isReachable)
+                            .map(FlatReachabilityExporter::stableMethodName)
+                            .sorted());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        BuildArtifacts.singleton().add(ArtifactType.BUILD_INFO, reachableTypesPath);
-        BuildArtifacts.singleton().add(ArtifactType.BUILD_INFO, instantiatedTypesPath);
-
-        try (BufferedWriter methods = new BufferedWriter(new FileWriter(reachableMethodsPath.toFile()))) {
-            Stream<String> names = accessImpl
-                    .getUniverse()
-                    .getMethods()
-                    .stream()
-                    .filter(AnalysisMethod::isReachable)
-                    .map(FlatReachabilityExporter::stableMethodName)
-                    .sorted();
-
-            for(String name : (Iterable<String>) names::iterator) {
-                methods.write(name);
-                methods.newLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        BuildArtifacts.singleton().add(ArtifactType.BUILD_INFO, reachableMethodsPath);
     }
 
     // Hotfix until c322e16d4b406f9e6b54a6188d19ef0c3a8b4535 gets merged